d3bd46fdf96f2211e58194c2a359aa1bc12a6ec2
BIRDRED <gustt103@uol.com.br>
2014-07-28 12:07:41 -0300

    Adjustment of the e-mail page

--- inc/email.php
+++ inc/email.php
@@ -4,20 +4,20 @@
  */
 
 return array(
-	'host' => 'smtp.conceito-online.com.br',
-	'user' => 'dev@conceito-online.com.br',
-	'pass' => 'conceito',
+	'host' => '',
+	'user' => '',
+	'pass' => '',
 	'port' => 587,
 	'encr' => '', // tsc
 
 	'to' => array(
-		'name' => 'Maria Cristina Matteotti Geraldo', // leave empty to use $titleSite
-		'email' => 'dev@conceito-online.com.br' // can be overridden by $toMail
+		'name' => '', // leave empty to use $titleSite
+		'email' => '' // can be overridden by $toMail
 	),
 
 	'from' => array(
 		'name' => '', // leave empty to use formName
-		'email' => 'dev@conceito-online.com.br' // leave empty to use formEmail
+		'email' => '' // leave empty to use formEmail
 	),
 
 	'copies' => array()
